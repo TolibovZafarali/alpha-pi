b6abce75e2bc1c2120bf5cac545fc7d773afef23
Zafarali Tolibov <zafaralit67@gmail.com>
2025-07-29 12:36:10 -0500

    Created Home.jsx and Login.jsx, removed unnecessary files

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h2 className="text-2xl font-bold text-center mt-10">Welcome to Alpha Pi</h2>
+    );
+}
+ 
+export default Home;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+const Login = () => {
+    return (
+        <h2 className="text-2xl font-bold text-center mt-10">Login Page</h2>
+    );
+}
+ 
+export default Login;
