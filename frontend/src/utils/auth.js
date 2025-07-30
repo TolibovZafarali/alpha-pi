b58408f0667fa0371479571b5d9da33c2a6b70e8
Zafarali Tolibov <zafaralit67@gmail.com>
2025-07-30 11:50:42 -0500

    Added userName to localStorage to handle Welcome message

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,15 +1,16 @@
 // Save ID and user type
-export const saveAuth = (id, userType) => {
-    localStorage.setItem("userId", id);
-    localStorage.setItem("userType", userType);
-  };
+export const saveAuth = (id, userType, userName) => {
+  localStorage.setItem("userId", id);
+  localStorage.setItem("userType", userType);
+  localStorage.setItem("userName", userName)
+};
   
-  // Get current auth info
-  export const getAuth = () => ({
-    id: localStorage.getItem("userId"),
-    type: localStorage.getItem("userType"),
-  });
-  
-  // Clear on logout
-  export const clearAuth = () => localStorage.clear();
-  
+// Get current auth info
+export const getAuth = () => ({
+  id: localStorage.getItem("userId"),
+  type: localStorage.getItem("userType"),
+  name: localStorage.getItem("userName")
+});
+
+// Clear on logout
+export const clearAuth = () => localStorage.clear();
