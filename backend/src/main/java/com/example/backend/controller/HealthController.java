8db481958ed87421cb838dfb9703eed08ce4a998
Zafarali Tolibov <zafaralit67@gmail.com>
2025-09-04 18:18:08 -0500

    Added Health dependency to check the health of the application with certain endpoints

---
+++ backend/src/main/java/com/example/backend/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.example.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/public")
+public class HealthController {
+    @GetMapping("/health")
+    public Map<String, Object> health() {
+        return Map.of("status", "OK");
+    }
+}
